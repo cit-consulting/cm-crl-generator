caffd6f5ac4f91420609d4b4d428edca7913e350
Vladimir Konkov <vkonkov@citc.ru>
2024-10-24 15:41:38 +0300

    add cmctl binary

--- docker/hooks/generate-crl.sh
+++ docker/hooks/generate-crl.sh
@@ -6,6 +6,7 @@ else
   # reconcile CA
   # cm-crl-generator.citc.dev/revoke = true
   echo implement me
+  cmctl status certificate -n ns-1 cert-1
   #  cm-crl-generator.citc.dev/revoke = false
   # ouput secert <ca-name>-crl : tls.revoked-certs tls.crl
   # annotation: cm-crl-generator.citc.dev/ca-fingerprint
